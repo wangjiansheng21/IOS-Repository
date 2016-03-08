1b027b652037a616f81103d4d130ff971c24b8e0
wangjiansheng <1823079199@qq.com>
2016-03-08 14:57:33 +0800

    wjs2
    wjs2

---
+++ wjs2/wjs2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wjs2
+//
+//  Created by AppleUser on 16/3/8.
+//  Copyright © 2016年 AppleUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
